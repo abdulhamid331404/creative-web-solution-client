2b167a095ad5a700ba71e02dcfba51f74c7669cf
abdulhamid331404 <freelancerhamid1@gmail.com>
2023-02-02 13:18:06 +0600

    create home page

---
+++ src/Pages/Home/TopBanner/TopBanner.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import BannerImg from '../../../assests/banner-image/banner.jpg'
+import BannerImg1 from '../../../assests/banner-image/banner1.jpg'
+import BannerImg2 from '../../../assests/banner-image/banner2.jpg'
+
+const TopBanner = () => {
+    return (
+        <div>
+            <div className="carousel h-96 w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={BannerImg1}
+                    className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={BannerImg} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={BannerImg1}  className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src={BannerImg2}  className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TopBanner;
